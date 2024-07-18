faf86672b86051caabb6f740bdfce8f99fc74a93
AceMoversKevin <154477478+AceMoversKevin@users.noreply.github.com>
2024-07-19 08:26:00 +1000

    Updated title

--- leadManagement.php
+++ leadManagement.php
@@ -174,7 +174,7 @@ $totalPages = ceil($totalLeads / $leadsPerPage);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex, nofollow">
-    <title>Leads Dashboard</title>
+    <title>Google Leads Dashboard</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
